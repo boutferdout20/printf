4c8ffe760f3819edcf51604e62e658b9ca9762e4
elmouy <elmoutaouakkaly@gmail.com>
2023-04-24 13:42:05 -0700

    all tasks

--- main.h
+++ main.h
@@ -16,7 +16,7 @@ int _printf(const char *format, ...);
 int _checker_p(char k, va_list args);
 int _str(char *m);
 int _pri_st(char *st);
-int _pri_ligne(char *k, char *c, char *C);
+int _pri_ligne(char *k, char __attribute__((unused)) *c, char *C);
 int _putchar(char);
 int _pri_st(char *st);
 int _pri_binary(unsigned int m);
