2f621005125b734184465150a6eb1fba7078eee3
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-04-24 13:33:59 -0700

    modification

--- main.c
+++ main.c
@@ -1,26 +1,28 @@
 #include "main.h"
 /**
-* main - enter les point
+* main - Entry point
 * Return: Always 0
 */
 int main(void)
 {
-int incr;
-unsigned int oui;
-len = _printf("Let's try to printf a simple sentence.\n");
-ui = (unsigned int)INT_MAX + 1024;
-_printf("Length:[%d,%i]\n", incr, incr);
-_printf("Negative:[%d]\n", -762534);
-_printf("Btissam & Youssef\n");
-_printf("%s\n", "Boutferdout and Yousseefff");
-_printf("%c\n", 'm');
-_printf("%d\n", 9);
-_printf("binary %b\n", 98);
-_printf("Unsigned:[%u]\n", 347698608);
-_printf("Unsigned octal:[%o]\n", oui);
-_printf("Unsigned hexadecimal:[%x, %X]\n", 32, 12);
-_printf("Character:[%c]\n", 'H');
-_printf("%%\n");
-_printf("%S\n", "Best\nSchool");
-return (0);
+	int len;
+	unsigned int ui;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+
+	_printf("Length:[%d,%i]\n", len, len);
+	_printf("Negative:[%d]\n", -762534);
+	_printf("Biti and yousef\n");
+	_printf("%s\n", "Basouma and yoyssef");
+	_printf("%c\n", 'm');
+	_printf("%d\n", 9);
+	_printf("binary %b\n", 98);
+	_printf("Unsigned:[%u]\n", 347698608);
+	_printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", 32, 12);
+	_printf("Character:[%c]\n", 'H');
+	_printf("%%\n");
+	_printf("%S\n", "Best\nSchool");
+	return (0);
 }
