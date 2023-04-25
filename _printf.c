9dc1f8f655255acaa8480c3ba55419a40bcdfe45
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-04-25 05:02:03 -0700

    test

--- _printf.c
+++ _printf.c
@@ -1,42 +1,42 @@
-#include <limits.h>
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+#include "main.h"
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (0);
+/**
+ * _printf - custom printf implementation
+ * @format: format string
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int a = 0;
+	int tgv = 0;
+	va_list list;
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	if (format[0] == ' ' && format[1] == '%' && format[2] == '\0')
+		return (-1);
+	va_start(list, format);
+	while (format && format[a])
+	{
+		if (format[a] == '%')
+		{
+			format++;
+			if (format[a] == '%')
+				tgv += _putchar('%');
+			else if (format[a] == '\0')
+			{
+				return (-1);
+			}
+			else
+				tgv += _checker_p(format[a], list);
+		}
+		else
+		{
+			tgv += _putchar(format[a]);
+		}
+		format++;
+	}
+	va_end(list);
+	return (tgv);
+
 }
