c8b6eb8f90fe4669114a9e6411aa6d9a66503f54
elmouy <elmoutaouakkaly@gmail.com>
2023-04-25 11:58:25 -0700

    all

--- _printf.c
+++ _printf.c
@@ -7,29 +7,36 @@
  */
 int _printf(const char *format, ...)
 {
-	int a = 0;
-	int tgv = 0;
-	va_list list;
+        int a = 0;
+        int tgv = 0;
+        va_list list;
 
-	va_start(list, format);
+        if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+                return (-1);
+        if (format[0] == ' ' && format[1] == '%' && format[2] == '\0')
+                return (-1);
+        va_start(list, format);
+        while (format && format[a])
+        {
+                if (format[a] == '%')
+                {
+                        format++;
+                        if (format[a] == '%')
+                                tgv += _putchar('%');
+                        else if (format[a] == '\0')
+                        {
+                                return (-1);
+                        }
+                        else
+                                tgv += _checker_p(format[a], list);
+                }
+                else
+                {
+                        tgv += _putchar(format[a]);
+                }
+                format++;
+        }
+        va_end(list);
+        return (tgv);
 
-	if (format == NULL)
-		return (-1);
-
-	while (format[a] != '\0')
-	{
-		if (format[a] == '%')
-		{
-			a++;
-			if (format[a] == '\0')
-				return (-1);
-			tgv += _checker_p(format[a], list);
-		}
-		else
-			tgv += _putchar(format[a]);
-
-		a++;
-	}
-	va_end(list);
-	return (tgv);
 }
